c93ad1032653a5ce5edd30b6731bca24967570d2
Andrei Fajardo <andrei@nerdai.io>
2024-02-17 01:52:46 -0500

    more initial ux

--- src/sbd/mod.rs
+++ src/sbd/mod.rs
@@ -1,8 +1,40 @@
-pub struct SBD {}
+pub struct Sbd {
+    pipeline: Vec<Box<dyn Process>>,
+}
 
-impl SBD {
-    pub fn split(&self, text: String) -> Vec<String> {
-        vec![text]
+pub trait Process {
+    fn process(&self, text: String) -> String;
+}
+
+pub struct ListItemReplacer {}
+
+impl Process for ListItemReplacer {
+    fn process(&self, mut text: String) -> String {
+        todo!()
+    }
+}
+
+pub struct AbbreviationReplacer {}
+
+impl Process for AbbreviationReplacer {
+    fn process(&self, text: String) -> String {
+        todo!()
+    }
+}
+
+impl Sbd {
+    fn process(&self, text: String) -> String {
+        // self.pipeline loop thru all processors and run .process()
+        todo!()
+    }
+
+    fn segment(&self, text: String) -> Vec<String> {
+        todo!()
+    }
+
+    pub fn get_sentences(&self, text: String) -> Vec<String> {
+        let process_text = self.process(text);
+        self.segment(process_text)
     }
 }
 
@@ -12,8 +44,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let sbd = SBD {};
-        let result = sbd.split(String::from("Hi!"));
-        println!("{:?}", result)
+        let sbd = Sbd {
+            pipeline: vec![
+                Box::new(ListItemReplacer {}),
+                Box::new(AbbreviationReplacer {}),
+            ],
+        };
+        // let result = sbd.segment(String::from("Hi!"));
+        assert!(true);
     }
 }
