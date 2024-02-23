8364016ba056c6140616b6e745d5e6abe7599706
CalculusC <chiho00hk@hotmail.com>
2024-02-23 15:32:31 -0500

    adding number rules

--- src/sbd/replacers/numbers.rs
+++ src/sbd/replacers/numbers.rs
@@ -5,13 +5,27 @@ use crate::utils::Rule;
 
 #[derive(Debug)]
 pub struct NumbersReplacer {
-    rules: [Rule; 1],
+    rules: [Rule; 5],
 }
 
 impl Default for NumbersReplacer {
     fn default() -> Self {
         Self {
-            rules: [Rule::new(r"\.(?=\d)", "∯", "PeriodBeforeNumberRule")],
+            rules: [
+                Rule::new(r"\.(?=\d)", "∯", "PeriodBeforeNumberRule"),
+                Rule::new(r"(?<=\d)\.(?=\S)", "∯", "NumberAfterPeriodBeforeLetterRule"),
+                Rule::new(
+                    r"(?<=\r\d)\.(?=(\s\S)|\))",
+                    "∯",
+                    "NewLineNumberPeriodSpaceLetterRule",
+                ),
+                Rule::new(r"(?<=^\d)\.(?=(\s\S)|\))", "∯", "StartLineNumberPeriodRule"),
+                Rule::new(
+                    r"(?<=^\d\d)\.(?=(\s\S)|\))",
+                    "∯",
+                    "StartLineTwoDigitNumberPeriodRule",
+                ),
+            ],
         }
     }
 }
@@ -43,6 +57,43 @@ mod tests {
         let numbers_replacer = NumbersReplacer::default();
         let mut text = String::from("$.50 and .50");
         text = numbers_replacer.process(text);
+        println!("{}", text);
         assert_eq!(text, "$∯50 and ∯50");
+    }
+
+    #[test]
+    fn test_number_after_period_before_letter_rule() {
+        let numbers_replacer = NumbersReplacer::default();
+        let mut text = String::from("$3.test");
+        text = numbers_replacer.process(text);
+        println!("{}", text);
+        assert_eq!(text, "$3∯test");
+    }
+
+    #[test]
+    fn test_new_line_number_period_space_letter_rule() {
+        let numbers_replacer = NumbersReplacer::default();
+        let mut text = String::from("\r1. Hello");
+        text = numbers_replacer.process(text);
+        println!("{}", text);
+        assert_eq!(text, "\r1∯ Hello");
+    }
+
+    #[test]
+    fn test_start_line_number_period_rule() {
+        let numbers_replacer = NumbersReplacer::default();
+        let mut text = String::from("1. Hello");
+        text = numbers_replacer.process(text);
+        println!("{}", text);
+        assert_eq!(text, "1∯ Hello");
+    }
+
+    #[test]
+    fn test_start_line_two_digit_number_period_rule() {
+        let numbers_replacer = NumbersReplacer::default();
+        let mut text = String::from("01. Hello");
+        text = numbers_replacer.process(text);
+        println!("{}", text);
+        assert_eq!(text, "01∯ Hello");
     }
 }
